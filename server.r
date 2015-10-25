1bf18df4b6c9b071a23c4b5a1568f4351a0053bc
ravirathi <ravirathi7@yahoo.co.in>
2015-10-25 12:27:05 -0500

    Update server.r

--- server.r
+++ server.r
@@ -10,12 +10,12 @@ shinyServer(
   function(input,output)
 {
   
-    #output$zip <- renderText(as.numeric(input$Zip))
+    
     output$chldhgt <- renderPrint({
       parent = as.numeric(input$Height)
       child <- predict(md,data.frame(parent))
       as.numeric(child)
       })
-    #output$reactve <- renderText({paste(input$Name,input$State)})
+    
   }
   )
